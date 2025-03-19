3edc074f401752fd20dbe5c57a9cb973bf247dc2
Mayank Roy <roy050703@gmail.com>
2025-03-19 12:43:48 +0530

    artistcard fixes

--- frontend/src/app/components/artistcard.jsx
+++ frontend/src/app/components/artistcard.jsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function ArtistCard({ artist }) {
     return (
-        <div className="flex flex-col gap-2 p-4 bg- text-white rounded-lg shadow-md">
-            <Image src="" alt={artist.Artist} className="rounded-lg" />
+        <div className="flex flex-col gap-2 p-4 text-white rounded-lg shadow-md justify-between mx-3">
+            <div className="imagehcontainer bg-[url(/placeholder.jpg)] bg-cover bg-center rounded-lg w-46 h-50">
+
+            </div>
             <div className="justify-between">
                 {artist.Genres && artist.Genres.map((genre, index) => (
                     <span key={index} className="text-sm text-gray-500">{genre}</span>
